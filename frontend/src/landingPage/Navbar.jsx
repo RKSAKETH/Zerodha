4346c842ea91ea5f1fd557369e959e395584fa7b
RKSAKETH <krishnasaketh566@gmail.com>
2025-07-13 20:46:10 +0530

    added navbar

--- frontend/src/landingPage/Navbar.jsx
+++ frontend/src/landingPage/Navbar.jsx
@@ -1,5 +1,17 @@
 export default function Navbar () {
     return (
-        <h1>Navbar</h1>
+        <header className="mb-15 mt-0.5 bg-white h-2 fixed">
+            <nav className="flex justify-end">
+                <img src="images/logo.svg" alt="logo" className="w-[8rem] h-auto mr-auto"></img>
+                <ul className="flex ">
+                    <li>Signup</li>
+                    <li>About</li>
+                    <li>Products</li>
+                    <li>Pricing</li>
+                    <li>Support</li>
+                </ul>
+            </nav>
+        </header>
+        
     )
 }
