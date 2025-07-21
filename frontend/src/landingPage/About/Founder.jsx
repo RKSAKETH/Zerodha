4a5e060f404a3c35831f96c6fe4baa186cecec29
RKSAKETH <krishnasaketh566@gmail.com>
2025-07-22 00:11:59 +0530

    added founder section in about page

--- frontend/src/landingPage/About/Founder.jsx
+++ frontend/src/landingPage/About/Founder.jsx
@@ -4,9 +4,9 @@ export default function Founder(){
             <h1 className="text-2xl text-center text-gray-700 font-medium mb-20">People</h1>
         <div className="grid grid-cols-[1fr_2fr] gap-x-30">
             <div>
-                <img src="images/nithinKamath.jpg" className="rounded-full h-auto w-2xs"></img>
-                <h4>Nithin Kamath</h4>
-                <h6>Founder, CEO</h6>
+                <img src="images/nithinKamath.jpg" className="rounded-full h-auto w-2xs mb-3"></img>
+                <h4 className="text-lg text-gray-800 mb-4 text-center">Nithin Kamath</h4>
+                <h6 className="text-sm text-gray-500 text-center">Founder, CEO</h6>
             </div>
             <div>
                 <p className="text-base/7 text-gray-700 mb-5">Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. 
