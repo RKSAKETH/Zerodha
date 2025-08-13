f45937421525820b13dca1dc8bc1fd5e59733ac4
RKSAKETH <krishnasaketh566@gmail.com>
2025-08-13 14:31:52 +0530

    initialized backend folder with index.js and established connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-const express = require('express')
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+const port =process.env.port || 8080 ;
+const url = process.env.MONGO_URL;
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port: ${port}`);
+    mongoose.connect(url);
+    console.log("DB Connected !");
+})
+
